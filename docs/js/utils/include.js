3f8d3b6022bd64eb7c80d4ee6871c87be27207fc
Agustín Borrego <45563306+agu-borrego@users.noreply.github.com>
2021-05-12 09:59:09 +0200

    Update include.js

--- docs/js/utils/include.js
+++ docs/js/utils/include.js
@@ -1,18 +1,16 @@
 "use strict";
 
-let include = (htmlFile, selector) => {
-    document.addEventListener("DOMContentLoaded", function () {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", htmlFile);
-        xhr.addEventListener("load", () => {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                document.querySelector(selector).innerHTML += xhr.responseText;
-            } else {
-                console.error(htmlFile + " does not exist");
-            }
-        });
-        xhr.send();
+function include(htmlFile, selector) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", htmlFile, false); // Becomes synchronous
+    xhr.addEventListener("load", () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.querySelector(selector).innerHTML += xhr.responseText;
+        } else {
+            console.error(htmlFile + " does not exist");
+        }
     });
+    xhr.send();
 }
 
 window.include = include;
